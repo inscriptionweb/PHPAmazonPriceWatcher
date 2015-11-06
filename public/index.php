7a71dde497d069138a6334e09666fecedc5482b6
Stephen Moon <stephen@logicalmoon.com>
2015-11-06 21:15:42 +0000

    Filling in details for bootstrap and re-done index.php.

--- public/index.php
+++ public/index.php
@@ -1,21 +1,10 @@
 <?php
 
-error_reporting(E_ALL|E_STRICT);
-ini_set('display_errors', true);
-date_default_timezone_set('Europe/London');
+    include '../application/bootstrap.php';
 
-$rootDir = dirname(dirname(__FILE__));
-set_include_path($rootDir . '/library' . PATH_SEPARATOR . get_include_path());
-
-require_once 'Zend/Loader.php';
-Zend_Loader::loadClass('Zend_Debug');
-Zend_Loader::loadClass('Zend_Controller_Front');
-
-// set up controller
-$frontController = Zend_Controller_Front::getInstance();
-$frontController->setControllerDirectory('../application/controllers');
-
-// run
-$frontController->dispatch();
+    $configSection = getenv('APW_CONFIG') ?
+        getenv('APW_CONFIG') : 'general';
+    $bootstrap = new Bootstrap($configSection);
+    $bootstrap->RunApp();
 
 ?>
