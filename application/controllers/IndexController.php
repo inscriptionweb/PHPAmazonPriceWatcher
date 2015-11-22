680080603430ce455b7d8aac139a9f60497d2f44
Stephen Moon <stephen@logicalmoon.com>
2015-11-22 23:15:23 +0000

    Used zf to create skeleton.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,10 +2,17 @@
 
 class IndexController extends Zend_Controller_Action
 {
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
     public function indexAction()
     {
-        $this->view->assign('title', 'Hello World');
+        // action body
     }
+
+
 }
 
-?>
